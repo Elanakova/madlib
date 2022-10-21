c6df892429d1dd4e972bd834dc8d95708ab73643
Elanakova Liubov <liubov.elanakova@posti.com>
2022-10-21 17:07:08 +0300

    first commit

---
+++ madlib.py
@@ -0,0 +1,13 @@
+noun1 = input("Noun: ")
+noun2 = input("Noun: ")
+noun3 = input("Noun: ")
+adj1 = input("Adjective: ")
+adj2 = input("Adjective: ")
+noun4 = input("Noun: ")
+noun5 = input("Noun: ")
+noun6 = input("Noun: ")
+madlib = f"The {noun1} and the {noun2} went to {noun3}\n In a {adj1} {adj2} boat,\n They took some {noun4}, \
+ and plenty of {noun5},\n Wrapped up in a five-pound note.\n The {noun1} looked up to the {noun6} above,\n \
+ And sang to a small guitar,\n 'O lovely {noun2}! O {noun2}, my love,\n What a beautiful {noun2} you are,\n \
+ You are, you are! What a beautiful {noun2} you are!'"
+print(madlib)
